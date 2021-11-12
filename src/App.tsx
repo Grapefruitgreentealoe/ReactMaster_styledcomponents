d9f6cfb03cb80d4d0397b69dffdff831e819b530
남궁은선 <nammugmuhg@gmail.com>
2021-11-12 18:50:43 +0900

    javascript syntax  : 디폴트값을 프롭스로 넘겨줄때 문법

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import Circle from "./Circle";
 function App() {
   return (
     <div>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <Circle bgColor="teal" borderColor="tomato"/>
+      <Circle bgColor="tomato" borderColor="tomato"/>
     </div>
   )
 }
--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,25 +1,33 @@
-import styled from "styled-components"
+import styled, { StyledInterface } from "styled-components"
 
 interface ContainerProps{
     bgColor: string;
+    borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
     width:200px;
     height : 200px;
     background-color: ${(props) => props.bgColor};
-    border-radius:100px; 
+    border-radius:100px;
+    border:1px solid ${(props)=>props.borderColor};
 `;
 //half the width 로 border-radius 를 지정하면 원이된다. 
 
 interface CircleProps {
     bgColor: string;
+    borderColor?: string;
+    text?: string;
 }
+//물음표를 붙이면 필수 prop이 아니다.
+
 // function Circle(props:CircleProps) {
 //     return <Container bgColor={props.bgColor}></Container>
 // }
-function Circle({bgColor}:CircleProps) {
-    return <Container bgColor={bgColor}></Container>
+function Circle({bgColor,borderColor, text = "default text"}:CircleProps) {
+    return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+        {text} 
+    </Container>
 }
 
 
