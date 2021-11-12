b037adb7e5162eb5e0bc2af385e3585449ce33d6
남궁은선 <nammugmuhg@gmail.com>
2021-11-12 18:05:42 +0900

    interface는 props(object)가 어떻게 보이는지를 설명해준다. 인터페이스에없는것은 인자로 들어갈수없다.

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,8 +1,16 @@
 import styled from "styled-components"
 
+interface ContainerProps{
+    bgColor: string;
+}
 
-
-const Container = styled.div``;
+const Container = styled.div<ContainerProps>`
+    width:200px;
+    height : 200px;
+    background-color: ${(props) => props.bgColor};
+    border-radius:100px; 
+`;
+//half the width 로 border-radius 를 지정하면 원이된다. 
 
 interface CircleProps {
     bgColor: string;
@@ -14,4 +22,8 @@ function Circle({bgColor}:CircleProps) {
     return <Container bgColor={bgColor}></Container>
 }
 
+
+
 export default Circle;
+
+//props를 interface로 정의해준다. object 가 어떻게 보이는지를 설명해준다.
